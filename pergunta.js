a90cb1ce7f68ced5ed2d39145c82c636d8a19b53
Franqueiro <00001088218143sp@al.educacao.sp.gov.br>
2024-11-27 16:22:16 -0300

    Create pergunta.js

---
+++ pergunta.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Formula 1',
+    'Qual o GP mais conhecido da formula 1?',
+    'O Grande Prêmio de Mônaco é a mais tradicional e a mais importante prova do calendário da Fórmula 1.'
+)
+
+criaCartao(
+    'Formula 1',
+    'Qual corredor da formula 1 mais ganhou premios?',
+    'Lewis Hamilton, sete vezes campeão mundial.'
+)
+
+criaCartao(
+    'Formula 1',
+    'Top 3 equipes mais famosas da formula 1?',
+    '1- Ferrari 2- McLare 3- Mercedes'
+)
+
+criaCartao(
+    'Formula 1',
+    'Qual piloto tem mais corridas na f1?',
+    'Ferando Alondo 396 (corridas)'
+)
+
+criaCartao(
+    'Formula 1',
+    'Quantas vezes Ayrton Senna foi campeão?',
+    '3 vezes ( em 88, 90 e 91)'
+                   
+)
+
+criaCartao(
+    'Formula 1',
+    'Quantas vezes Charles Leclerc ganhou?',
+    'Seis vezes'
+)
+
+criaCartao(
+    'Formula 1',
+    ' Qual piloto mais conhecido da Ferrari?',
+    'Charles Leclerc'
+)
+
+criaCartao(
+    'Formula 1',
+    ' Qual é o piloto mais famoso do mundo?',
+    'Lewis Hamilton'
+)
+
+criaCartao(
+    'Formula 1',
+    'Quais sao os pilotos que correrao para a ferrari em 2025?',
+    'Charles Leclerc e Lewis Hamilton'
+)
